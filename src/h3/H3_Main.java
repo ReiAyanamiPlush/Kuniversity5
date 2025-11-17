9e6be19c1a2f477429961db9aa007a633347eb87
ReiAyanamiPlush <bozorgmehrkazemieh@gmail.com>
2025-11-17 14:20:04 +0100

    h3

--- src/h3/H3_Main.java
+++ src/h3/H3_Main.java
@@ -5,11 +5,11 @@ public class H3_Main {
     public static void main(String[] args) {
 
         int[][] einheiten = new int[][]{{50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-        int input = 7017;
+        int input = 57018;
 
         while (input > 0) {
 
-            for (int i = 0; i <= 14; i++) {
+            for (int i = 0; i <= einheiten[0].length-1; i++) {
 
                 einheiten[1][i] = input / einheiten[0][i];
                 input = input % einheiten[0][i];
